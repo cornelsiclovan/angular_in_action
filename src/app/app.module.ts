48f6fa100aee689c31ae01fdce088020adb2dfd0
cornel <cornel.siclovan@gmail.com>
2020-04-29 16:58:22 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
+import { StocksService } from './services/stocks.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SummaryComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [StocksService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
